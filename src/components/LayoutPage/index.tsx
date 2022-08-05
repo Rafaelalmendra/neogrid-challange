36deded8ce8f1f8c5d7af6ddd009baaf2526245e
Rafael Almendra <rafaelalmendra28@gmail.com>
2022-08-05 18:44:14 -0300

    refactor: 💡 componentize the navbar

--- src/components/LayoutPage/index.tsx
+++ src/components/LayoutPage/index.tsx
@@ -1,109 +1,23 @@
-import Link from "next/link";
-import Image from "next/image";
-import { useRouter } from "next/router";
-import { useMedia } from "use-media";
-
 //design-system
-import { Layout, Dropdown, Menu, Space } from "antd";
+import { Layout } from "antd";
 import { Content, Footer, Header } from "antd/lib/layout/layout";
 
 //icons
-import { HeartOutlined, MenuOutlined } from "@ant-design/icons";
+import { HeartOutlined } from "@ant-design/icons";
 
 //styles
 import styles from "./styles.module.less";
+import { Navbar } from "../Navbar";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
-interface LinksProps {
-  url: string;
-  name: string;
-}
-
-const links: LinksProps[] = [
-  {
-    url: "/",
-    name: "Home",
-  },
-  {
-    url: "/pokemons",
-    name: "Pokémons",
-  },
-];
-
-const menu = (
-  <Menu
-    items={[
-      {
-        key: "1",
-        label: (
-          <Link href="/">
-            <a>Home</a>
-          </Link>
-        ),
-      },
-      {
-        key: "2",
-        label: (
-          <Link href="/pokemons">
-            <a>Pokémon</a>
-          </Link>
-        ),
-      },
-    ]}
-  />
-);
-
 export const LayoutPage = ({ children }: LayoutProps) => {
-  const router = useRouter();
-  const isMobile = useMedia({ maxWidth: "768px" });
-
   return (
     <Layout className={styles.containerLayout}>
       <Header className={styles.header}>
-        <div className={styles.headerContent}>
-          <div className={styles.logoContainer}>
-            <Link href="/">
-              <a>
-                <Image
-                  src="/images/logo.png"
-                  alt="Logo Principal do Pokémon Universe"
-                  layout="fill"
-                />
-              </a>
-            </Link>
-          </div>
-
-          {!isMobile ? (
-            <ul>
-              {links.map((link) => (
-                <li key={link.url}>
-                  <Link href={link.url}>
-                    <a
-                      style={{
-                        color: `${
-                          router.pathname === link.url ? "#3D67BA" : "#3a3a3c"
-                        }`,
-                      }}
-                    >
-                      {link.name}
-                    </a>
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <Dropdown overlay={menu} trigger={["click"]}>
-              <a onClick={(e) => e.preventDefault()}>
-                <Space>
-                  <MenuOutlined />
-                </Space>
-              </a>
-            </Dropdown>
-          )}
-        </div>
+        <Navbar />
       </Header>
 
       <Content className={styles.containerContent}>
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,101 @@
+import Link from "next/link";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import useMedia from "use-media";
+
+//design-system
+import { Dropdown, Menu, Space } from "antd";
+
+//styles
+import styles from "./styles.module.less";
+
+//icons
+import { MenuOutlined } from "@ant-design/icons";
+
+interface LinksProps {
+  url: string;
+  name: string;
+}
+
+const links: LinksProps[] = [
+  {
+    url: "/",
+    name: "Home",
+  },
+  {
+    url: "/pokemons",
+    name: "Pokémons",
+  },
+];
+
+const menu = (
+  <Menu
+    items={[
+      {
+        key: "1",
+        label: (
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        ),
+      },
+      {
+        key: "2",
+        label: (
+          <Link href="/pokemons">
+            <a>Pokémon</a>
+          </Link>
+        ),
+      },
+    ]}
+  />
+);
+
+export const Navbar = () => {
+  const router = useRouter();
+  const isMobile = useMedia({ maxWidth: "768px" });
+
+  return (
+    <div className={styles.headerContent}>
+      <div className={styles.logoContainer}>
+        <Link href="/">
+          <a>
+            <Image
+              src="/images/logo.png"
+              alt="Logo Principal do Pokémon Universe"
+              layout="fill"
+            />
+          </a>
+        </Link>
+      </div>
+
+      {!isMobile ? (
+        <ul>
+          {links.map((link) => (
+            <li key={link.url}>
+              <Link href={link.url}>
+                <a
+                  style={{
+                    color: `${
+                      router.pathname === link.url ? "#3D67BA" : "#3a3a3c"
+                    }`,
+                  }}
+                >
+                  {link.name}
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <Dropdown overlay={menu} trigger={["click"]}>
+          <a onClick={(e) => e.preventDefault()}>
+            <Space>
+              <MenuOutlined />
+            </Space>
+          </a>
+        </Dropdown>
+      )}
+    </div>
+  );
+};
